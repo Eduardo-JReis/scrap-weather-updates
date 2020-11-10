38b4affcc12377ecefb99ef0a224b4acc471eca8
Eduardo-JReis <edu.publicidade81@gmail.com>
2020-11-09 23:16:40 -0300

    script file added

---
+++ index.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+
+search = "Weather in Salvador"
+
+url = f"https://www.google.com/search?&q={search}"
+
+r = requests.get(url)
+
+s = BeautifulSoup(r.text, "html.parser")
+
+updates = s.find("div",class_="BNeawe").text
+
+print(updates)
